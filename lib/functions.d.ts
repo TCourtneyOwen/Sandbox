d72f6a1927908014f63f2a3a31421b24e0ec681a
Courtney Owen <cowen@microsoft.com>
2018-07-31 15:36:23 -0700

    Make additional changes to custom functions sandbox repo

--- lib/functions.d.ts
+++
@@ -1,4 +0,0 @@
-declare var CustomFunctionMappings: any;
-declare function add(first: number, second: number): number;
-declare function addAsync(first: number, second: number): Promise<number>;
-declare function increment(incrementBy: number, callback: any): void;
