dcd5fc77e454dc10b44162ff38b4e69c791334ab
Courtney Owen <cowen@microsoft.com>
2018-10-31 12:50:01 -0700

    make small test change

--- test/test.js
+++ test/test.js
@@ -5,11 +5,7 @@ describe('Custom Functions', function() {
     describe('Custom Functions Samples', function() {
       it('should verify sample custom functions work as expected', function() {
         const addResult = customFunctions.add(5,9);  
-        assert.strictEqual(addResult, 1014);
-      });
-      it('should verify sample custom functions work as expected', function() {
-        const addResult = customFunctions.add(5,5);  
-        assert.strictEqual(addResult, 1010);
+        assert.strictEqual(addResult, 14);
       });
     });
   });
