0b860dd8cc0d2c469b690853f1db6444d05ee4ad
TCourtneyOwenTest <44653580+TCourtneyOwenTest@users.noreply.github.com>
2018-10-31 13:50:26 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ const assert = require('assert');
 describe('Custom Functions', function() {
     describe('Custom Functions Samples', function() {
       it('should verify sample custom functions work as expected', function() {
-        const addResult = customFunctions.add(5,6);  
-        assert.strictEqual(addResult, 1011);
+        const addResult = customFunctions.add(5,7);  
+        assert.strictEqual(addResult, 1012);
       });
       it('should verify sample custom functions work as expected', function() {
         const addResult = customFunctions.add(5,5);  
