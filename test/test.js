9b3b3d63db6334de3807bbe60d01534f86b05588
Courtney Owen <cowen@microsoft.com>
2018-11-01 14:15:17 -0700

    updates custom functions tests. Closes #1

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ const assert = require('assert');
 describe('Custom Functions', function() {
     describe('Custom Functions Samples', function() {
       it('should verify sample custom functions work as expected', function() {
-        const addResult = customFunctions.add(5,5);  
-        assert.strictEqual(addResult, 10);
+        const addResult = customFunctions.add(5,6);  
+        assert.strictEqual(addResult, 11);
       });
     });
   });
