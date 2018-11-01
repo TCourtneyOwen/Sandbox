508da4b153b8e312da2e03ee523b2c7ef546a78b
Courtney Owen <cowen@microsoft.com>
2018-11-01 14:50:02 -0700

    updates custom functions tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ describe('Custom Functions', function() {
     describe('Custom Functions Samples', function() {
       it('should verify sample custom functions work as expected', function() {
         const addResult = customFunctions.add(5,7);  
-        assert.strictEqual(addResult, 13);
+        assert.strictEqual(addResult, 12);
       });
     });
   });
