76b526c893aca124630cdd44331cdc109bb0ce92
Courtney Owen <cowen@microsoft.com>
2018-11-01 08:43:56 -0700

    make tests change

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ const assert = require('assert');
 describe('Custom Functions', function() {
     describe('Custom Functions Samples', function() {
       it('should verify sample custom functions work as expected', function() {
-        const addResult = customFunctions.add(5,7);  
-        assert.strictEqual(addResult, 1012);
+        const addResult = customFunctions.add(5,8);  
+        assert.strictEqual(addResult, 13);
       });
       it('should verify sample custom functions work as expected', function() {
         const addResult = customFunctions.add(5,5);  
